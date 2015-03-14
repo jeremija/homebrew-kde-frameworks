19e33ebd1e72368f8a3ed69711a01a171649f0e9
You-Sheng Yang (楊有勝) <vicamo@gmail.com>
2015-03-15 00:22:31 +0800

    kate: upgrade to 15.03.90

--- kate.rb
+++ kate.rb
@@ -1,8 +1,8 @@
 require "formula"
 
 class Kate < Formula
-  url "http://download.kde.org/stable/applications/14.12.2/src/kate-14.12.2.tar.xz"
-  sha1 "bc3a5b1213fbe96756261daadf9ff8f94e74fd9c"
+  url "http://download.kde.org/unstable/applications/15.03.90/src/kate-15.03.90.tar.xz"
+  sha1 "d9dfe73e9682c8c64a68cab08a89fc7d6817cd3a"
   homepage "http://www.kde.org/"
 
   head "git://anongit.kde.org/kate.git"
